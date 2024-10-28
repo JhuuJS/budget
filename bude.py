f0bd4a954234a6cf7438388b6cda089ec19d9fe0
“JhuuJS” <joonatan.surakka@edu.hyria.fi>
2024-10-28 13:49:48 +0200

    hmm

--- bude.py
+++ bude.py
@@ -1,9 +1,25 @@
+def __intit__(self):
+    self.Vuokra = 0
+    self.Vakuutukset = 0
+    self.Kouluruokailu = 0
+    self.Sahko = 0
+    self.Liikkuminen = 0
+    self.Muut = 0
 
-Vuokra = int(input("Paljon rahaa vuokraan kuussa?"))
-Vakuutukset = int(input("Paljon rahaa vakuutuksiin kuussa?"))
-Kouluruokailu = int(input("Paljon rahaa kouluruokailuun kuussa?"))
-Sahko = int(input("Paljon rahaa sähköön kuussa="))
-Liikkuminen = int(input("Paljon rahaa liikkumiseen kuussa?"))
-Muut = int(input("Muita kuukausittaisia menoja?"))
-menot = Vuokra + Vakuutukset + Kouluruokailu + Sahko + Liikkuminen
-print("Kuukausi menosi ovat", menot)
+
+def save(item):
+    saved = item
+    return saved
+
+
+
+while True:
+    Vuokra = int(input("Paljon rahaa vuokraan kuussa?"))
+    Vakuutukset = int(input("Paljon rahaa vakuutuksiin kuussa?"))
+    Kouluruokailu = int(input("Paljon rahaa kouluruokailuun kuussa?"))
+    Sahko = int(input("Paljon rahaa sähköön kuussa="))
+    Liikkuminen = int(input("Paljon rahaa liikkumiseen kuussa?"))
+    Muut = int(input("Muita kuukausittaisia menoja?"))
+    menot = Vuokra + Vakuutukset + Kouluruokailu + Sahko + Liikkuminen
+    print(menot)
+    choice = input("'edit' tai 'copy' tai ")
